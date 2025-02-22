a53769bbccd2a547aaf46cbc908527031db6c479
Vivek Somasundaram <viveksomasundaram@Viveks-MacBook-Air.local>
2025-02-22 20:52:44 +0530

    Add the test for a single number and implement the logic to return the same number.

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,5 +1,6 @@
 class StringCalculator
     def self.add(numbers)
-      0 if numbers.empty?
+        return 0 if numbers.empty?
+        numbers.to_i
     end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe StringCalculator do
     it 'returns 0 for an empty string' do
         expect(StringCalculator.add("")).to eq(0)
     end
+
+    it 'returns the number itself for a single number' do
+        expect(StringCalculator.add("1")).to eq(1)
+    end
 end
